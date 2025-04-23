9afc36dde6af17de4ac660a4f0f9ff0633752f07
Limo123123 <90343619+Limo123123@users.noreply.github.com>
2025-04-23 17:55:05 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="gpiocom",
-    version="0.1.2",
+    version="0.1.3",
     author="Florian Töns",
     description="Schnelle GPIO-Kommunikation für Raspberry Pi",
     long_description=open('README.md').read(),
